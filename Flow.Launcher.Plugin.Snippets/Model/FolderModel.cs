a977650b41c6d3e64b0f53d1e7571414b5ba8903
YuXiaoyao <zxyfuncy@gmail.com>
2026-04-20 14:00:47 +0800

    add folder ui panel

---
+++ Flow.Launcher.Plugin.Snippets/Model/FolderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Flow.Launcher.Plugin.Snippets.Model;
+
+public class FolderModel
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public long OrderNum { get; set; }
+
+    public DateTime CreateTime { get; set; }
+
+    public DateTime UpdateTime { get; set; }
+}
--- Flow.Launcher.Plugin.Snippets/SettingWindow.xaml.cs
+++ Flow.Launcher.Plugin.Snippets/SettingWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
+using Flow.Launcher.Plugin.Snippets.Model;
+using JetBrains.Annotations;
 
 namespace Flow.Launcher.Plugin.Snippets;
 
@@ -12,13 +15,55 @@ public partial class SettingWindow : Window
     private readonly PluginInitContext _context;
     private readonly SnippetManage _snippetManage;
 
+    private List<FolderModel> _folders;
+
     public SettingWindow(PluginInitContext context, SnippetManage snippetManage)
     {
         _context = context;
         _snippetManage = snippetManage;
         InitializeComponent();
         // ComboBoxFilterType.SelectedIndex = 0;
+        _folders = _getFolders();
     }
+
+    #region 数据加载
+
+    private List<FolderModel> _getFolders([CanBeNull] string name = null)
+    {
+        return new List<FolderModel>()
+        {
+            new FolderModel()
+            {
+                Id = 1,
+                OrderNum = 1,
+                Name = "Java",
+                UpdateTime = DateTime.Now,
+            },
+            new FolderModel()
+            {
+                Id = 2,
+                OrderNum = 2,
+                Name = "Rust",
+                UpdateTime = DateTime.Now,
+            },
+            new FolderModel()
+            {
+                Id = 3,
+                OrderNum = 3,
+                Name = "GoLang",
+                UpdateTime = DateTime.Now,
+            },
+            new FolderModel()
+            {
+                Id = 4,
+                OrderNum = 4,
+                Name = "Python",
+                UpdateTime = DateTime.Now,
+            }
+        };
+    }
+
+    #endregion
 
     private void OnLoaded(object sender, RoutedEventArgs e)
     {
